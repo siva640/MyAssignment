0409923c89138529746ac521d7ce27f67ece22ea
siva640 <sivaselvamoorthy@gmail.com>
2025-05-25 10:25:32 +0530

    Day 1

---
+++ 01-firstname.js
@@ -0,0 +1,17 @@
+
+
+let firstName = "siva";
+console.log("firstName:", firstName, "| Type:", typeof firstName);
+
+let CompanyName ="CI";
+console.log("CompanyName:", CompanyName, "| Type:", typeof CompanyName);
+
+
+let mobileNumber = 9876543210;
+console.log("mobileNumber:", mobileNumber, "| Type:", typeof mobileNumber);
+
+let isAutomation ="True"
+console.log("isAutomation:", isAutomation, "| Type:", typeof isAutomation);
+
+let hasplywright;
+console.log("hasplaywright:",hasplywright,"|Type:",typeof hasplywright);
